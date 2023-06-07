8373fed659d0af897130791cb35e15b25bb71086
NHOY <NHOY@2210DR000919>
2023-06-07 19:53:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp57/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp57
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tryCount = 3;
+            string password = "secret";
+            string userInput;
+
+            Console.WriteLine("Когда посох - сильнее меча?");
+
+            for (int i = 0; i < tryCount; i++)
+            {
+                Console.Write("Введите парорль:");
+                userInput = Console.ReadLine();
+
+                if (password == userInput)
+                {
+                    Console.WriteLine("Ответ прост - когда он в умелых руках.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("У вас осталось - " + (tryCount - i - 1) + " попыток.");
+                }
+            }
+        }
+    }
+}
